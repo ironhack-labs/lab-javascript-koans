0f8915b36575048223ed60ac3ad5871db9222736
Gabriel-Rivera <leirbag.evil.z@gmail.com>
2018-02-01 09:05:44 -0600

    KOANS

---
+++ spec/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+var user={
+    "login": "Gabriel-Rivera",
+    "id": 35468675,
+    avatar_url: "https://avatars1.githubusercontent.com/u/35468675?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Gabriel-Rivera",
+    "html_url": "https://github.com/Gabriel-Rivera",
+    "followers_url": "https://api.github.com/users/Gabriel-Rivera/followers",
+    "following_url": "https://api.github.com/users/Gabriel-Rivera/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Gabriel-Rivera/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Gabriel-Rivera/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Gabriel-Rivera/subscriptions",
+    "organizations_url": "https://api.github.com/users/Gabriel-Rivera/orgs",
+    "repos_url": "https://api.github.com/users/Gabriel-Rivera/repos",
+    "events_url": "https://api.github.com/users/Gabriel-Rivera/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Gabriel-Rivera/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": null,
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "public_repos": 15,
+    "public_gists": 0,
+    "followers": 4,
+    "following": 4,
+    "created_at": "2018-01-15T20:33:52Z",
+    "updated_at": "2018-01-25T17:23:56Z"
+  }
+
+  $('.user-info img').prop("src",user.avatar_url);
+  
+
+
+
+
+
+
+
+});
