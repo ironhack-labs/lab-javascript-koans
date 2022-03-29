ebea2f8a9f3044e9d9fa8a6674504f4f7df88e5c
Gabriela <s.gabriela.bv96@gmail.com>
2022-03-29 16:31:18 +0200

    [MAD PP] Iñigo Malluguiza &&  Gabriela Benalcazar

--- tests/koans.spec.js
+++ tests/koans.spec.js
@@ -26,18 +26,18 @@ describe('the JavaScript language', () => {
 
     it('surprises me, NaN is not comparable with NaN', () => {
       expect(5 / 'a').toEqual(5 / 'a');
-      // expect(typeof NaN).toEqual();
+      expect(undefined).toEqual();
       expect(isNaN(5 / 'a')).toBeTruthy();
     });
 
     it('considers an empty string to be falsy', () => {
-      //expect("" == false).toBe......();// Truthy or Falsy
-      //expect("" === false).toBe.....();// Truthy or Falsy
+      expect("" == false).toBe(true);// Truthy or Falsy
+      expect("" === false).toBe(false);// Truthy or Falsy
     });
 
     it('considers zero to be falsy', () => {
-      //expect(0 == false).toBe......();// Truthy or Falsy
-      //expect(0 === false).toBe.....();// Truthy or Falsy
+      expect(0 == false).toBe(true);// Truthy or Falsy
+      expect(0 === false).toBe(false);// Truthy or Falsy
     });
 
     it('considers nulls to be falsy', () => {
@@ -50,18 +50,18 @@ describe('the JavaScript language', () => {
         result = false;
       }
 
-      //expect(result == false).toBe......();// Truthy or Falsy
-      //expect(null === false).toBe.....();// Truthy or Falsy
-      //expect(null == false).toBe....();// Truthy or Falsy
+      expect(result == false).toBe(true);// Truthy or Falsy
+      expect(null === false).toBe(false);// Truthy or Falsy
+      expect(null == false).toBe(false);// Truthy or Falsy
     });
 
     it('knows the type of a function', () => {
       function x() {
-        // ...
+        return ""
       }
 
-      expect(typeof x).toBe('function');
-      //expect(typeof(xxx)).toBe('...');
+      // expect(typeof x).toBe('function');
+      // expect(typeof(xxx)).toBe('');
     });
 
     it('has arrays and they can contain anything inside', () => {
@@ -75,13 +75,12 @@ describe('the JavaScript language', () => {
         ['g', 7, 8]
       ];
 
-      /*
-      expect(arr[1]).toEqual();
-      expect(arr[4]).toEqual();
-      expect(arr[6]).toEqual();
-      expect(arr[9]).toEqual();
-      expect(matrix[0][2]).toEqual();
-      */
+      expect(arr[1]).toEqual(2);
+      expect(arr[4]).toEqual(5);
+      expect(arr[6]).toEqual(undefined);
+      expect(arr[9]).toEqual(6);
+      expect(matrix[0][2]).toEqual("c");
+      
     });
 
     it('may contain functions inside arrays', () => {
@@ -93,14 +92,14 @@ describe('the JavaScript language', () => {
         }
       ];
 
-      //expect(arr[2](1)).toEqual();
+      expect(arr[2](1)).toEqual(4);
     });
 
     it('concatenate arrays - well, kind of', () => {
       const a = [1, 2, 3];
       const b = [4, 5, 6];
 
-      //expect(a + b).toEqual();
+      expect(a.concat(b)).toEqual([...a,...b]);
     });
 
     it('joins arrays and strings', () => {
