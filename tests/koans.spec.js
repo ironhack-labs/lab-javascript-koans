832bfd63d0aef2f2bee3842db9f3390a16329b4f
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-07-10 21:40:12 +0200

    done

--- tests/koans.spec.js
+++ tests/koans.spec.js
@@ -26,7 +26,7 @@ describe('the JavaScript language', () => {
 
     it('surprises me, NaN is not comparable with NaN', () => {
       expect(5 / 'a').toEqual(5 / 'a');
-      // expect(typeof NaN).toEqual();
+      expect(typeof NaN).toEqual();
       expect(isNaN(5 / 'a')).toBeTruthy();
     });
 
